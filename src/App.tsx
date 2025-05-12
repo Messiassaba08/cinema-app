d22b2eee296bd5b91bede516a6c3c9830020827a
Messias Sabadini <messiassaba08@gmail.com>
2025-05-12 20:34:05 -0300

    Inicio inserção sites de escolha de cadeiras

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { movies } from "./data/movies"; // Presumo que você tenha um arquivo de dados de filmes
+import Site1 from "./pages/1_ticket";
+import Site2 from "./pages/2_ticket";
+import Site3 from "./pages/3_ticket";
 import "./App.css";
-import { movies } from "./data/movies";
 
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <h1>Filmes em Cartaz</h1>
-      <div style={{ display: "flex", gap: "20px", flexWrap: "wrap" }}>
-        {movies.map((movie) => (
-          <div key={movie.id} style={{ textAlign: "center" }}>
-            <img src={movie.posterUrl} alt={movie.title} width="150" />
-            <p>{movie.title}</p>
-          </div>
-        ))}
+    <Router>
+      <div className="app-container">
+        <h1>Filmes em Cartaz</h1>
+        <div className="movie-grid">
+          {movies.map((movie) => (
+            <div key={movie.id} className="movie-card">
+              {/* Transformar a imagem em link */}
+              <Link to={`/movie/${movie.id}`} className="movie-link">
+                <img src={movie.posterUrl} alt={movie.title} width="150" />
+              </Link>
+
+              {/* Transformar o nome em link */}
+              <Link to={`/movie/${movie.id}`} className="movie-link">
+                <p>{movie.title}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+
+      {/* Definindo as rotas para os filmes */}
+      <Routes>
+        <Route path="/movie/1" element={<Site1 />} />
+        <Route path="/movie/2" element={<Site2 />} />
+        <Route path="/movie/3" element={<Site3 />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/1_ticket.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Site1 = () => {
+  return (
+    <div>
+      <h2>Detalhes do Filme 3</h2>
+      <p>Aqui estão os detalhes do filme 3...</p>
+    </div>
+  );
+};
+
+export default Site1;
+
+export {};
