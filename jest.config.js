dd0947ce426a536b11c968e3473833ec6d64ba8f
Wallace <wallacepereira2812@gmail.com>
2025-06-06 00:19:28 -0300

    fixing test workflow

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     preset: 'ts-jest',
-    testEnvironment: 'node',
-    collectCoverage: true,
-    coverageDirectory: 'coverage',
-    coverageReporters: ['text', 'lcov'],
-};
+    testEnvironment: 'jsdom',
+    moduleNameMapper: {
+      '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    },
+    transformIgnorePatterns: [
+      'node_modules/(?!(react-router-dom)/)',
+    ],
+    setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  };
+  
